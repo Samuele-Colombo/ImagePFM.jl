04799c7120c029379c464de168c12d5d9ab6823b
Samuele-Colombo <samuele.colombo1@studenti.unimi.it>
2021-04-11 17:50:38 +0200

    Compact field name

--- src/exceptions.jl
+++ src/exceptions.jl
@@ -1,3 +1,4 @@
 struct InvalidPfmFileFormat <: Exception
+    msg::String
 end
-Base.showerror(io::IO, e::InvalidPfmFileFormat) = print(io, typeof(e), ": ", e.message)
+Base.showerror(io::IO, e::InvalidPfmFileFormat) = print(io, typeof(e), ": ", e.msg)
